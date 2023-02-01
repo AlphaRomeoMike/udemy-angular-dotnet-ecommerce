096db0401acc789bd243645a34624a78fb1b7677
Abdul Rafay Modi <74718326+AlphaRomeoMike@users.noreply.github.com>
2023-02-02 02:26:40 +0500

    - Dangerous commit ⚡💀

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -9,11 +9,12 @@ import {
 import { catchError, Observable, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr/public_api';
+import { HotToastService } from '@ngneat/hot-toast/public-api';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router, private toastr: ToastrService) { }
+  constructor(private router: Router, private toastr: HotToastService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).
@@ -22,6 +23,7 @@ export class ErrorInterceptor implements HttpInterceptor {
           if (error) {
             if (error.status == 404) {
               this.router.navigateByUrl('/not-found');
+              this.toastr.error('Resource not found', { theme: 'snackbar', position: 'top-right' })
             }
             if (error.status === 500) {
               this.router.navigateByUrl('/server-error');
