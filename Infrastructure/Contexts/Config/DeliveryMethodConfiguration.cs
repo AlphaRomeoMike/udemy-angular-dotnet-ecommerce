e89280b4557824ba6785a389186a2c2ed0f6e77e
Abdul Rafay Modi <74718326+AlphaRomeoMike@users.noreply.github.com>
2023-02-08 01:28:33 +0500

    - WIP Orders API 🛒🛍️

---
+++ Infrastructure/Contexts/Config/DeliveryMethodConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Contexts.Config
+{
+    internal class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(i => i.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ Infrastructure/Contexts/Config/OrderConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Contexts.Config
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(o => o.ShippedToAddress, a => a.WithOwner());
+            builder.Navigation(a => a.ShippedToAddress).IsRequired();
+            builder.Property(s => s.Status)
+                .HasConversion(o => o.ToString(), o => (OrderStatus) Enum.Parse(typeof(OrderStatus), o));
+            builder.HasMany(o => o.OrderItems).WithOne().OnDelete(DeleteBehavior.Cascade);
+            builder.Property(i => i.SubTotal).HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
